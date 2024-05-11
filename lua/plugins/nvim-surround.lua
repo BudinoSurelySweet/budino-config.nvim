ebfb44c7404df3a5ad7b4c3e9fcf0995b734a4ce
Marco Molossi <molossi.marco1906@gmail.com>
2024-05-12 01:20:08 +0200

    Added nvim-surround plugin

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,20 @@
+-- =========================================== --
+-- ============== nvim-surround ============== --
+-- =========================================== --
+
+-- Surround selections, stylishly 😎
+-- https://github.com/kylechui/nvim-surround
+
+-- Setup
+local plugin = {
+	"kylechui/nvim-surround",
+	version = "*",
+	event = "VeryLazy",
+}
+
+-- Configuration
+function plugin.config()
+	return {}
+end
+
+return plugin
