2cf98ef61502133ab367718a60b566a55fa01307
Marco Molossi <molossi.marco1906@gmail.com>
2024-05-13 23:20:26 +0200

    Added fugitive plugin

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,20 @@
+-- ====================================== --
+-- ============== Fugitive ============== --
+-- ====================================== --
+
+-- Fugitive is the premier Vim plugin for Git. Or maybe it's the premier Git plugin for Vim? Either way, it's "so awesome, it should be illegal". That's why it's called Fugitive.
+-- https://github.com/tpope/vim-fugitive
+
+-- Setup
+local plugin = {
+	"tpope/vim-fugitive",
+}
+
+-- Configuration
+function plugin.config()
+	return {
+		-- Here go the plugin's configuration
+	}
+end
+
+return plugin
